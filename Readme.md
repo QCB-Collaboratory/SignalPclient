4046c0b0dbd684b6286373db05389c0ab41cce19
Thiago Mosqueiro <thiago.mosqueiro@gmail.com>
2017-12-11 15:01:28 -0800

    First after Hackathon

--- Readme.md
+++ Readme.md
@@ -1,7 +1,31 @@
-# Small Python Client for SignalP Server
+# Python Client for the SignalP Server
+
+<img src="resources/qcbCollaboratory_logo.png" height="50"/>
+
+signalPclient is small Python script that automates the submission of a sequence databank to ServerP 4.1 Server, a web service that detects and predicts the cleavage points of signal peptides for Gram-positive prokaryotes, Gram-negative prokaryotes, and eukaryotes. SignalP Server is hosted by the Center for Biological Sequence Analysis at the Technical University of Denmark (DTU):
+
+http://www.cbs.dtu.dk/services/SignalP/
+
+We started developing signalPclient during the [Python Hackathon](https://github.com/thmosqueiro/UCLA-Collaboratory_Hackathon/blob/master/Materials_Resources/Problem-4/Readme.md), hosted by the [QCB Collaboratory](https://qcb.ucla.edu/collaboratory/) at UCLA. SignalP Server was originally proposed on a [Nature Methods paper](https://www.nature.com/articles/nmeth.1701) by TN Petersen, S Brunak, G von Heijne and H Nielsen.
 
 
-SignalP Server url: http://www.cbs.dtu.dk/services/SignalP/
+<img src="./resources/scheme_signalPclient.png" width="600" />
+
+
+## Dependencies
+
+Other than the standard Python libraries, signalPclient only requires:
+
+* pyfasta
+* Mechanize (Python 2) or MechanicalSoup (Python 3)
+
+
+## People involved
+
+* Xiaofei Lin
+* Anela Tosevska
+* Cheng Chen
+* Thiago Mosqueiro
 
 
 ## License to use
--- SignalPclient.py
+++ SignalPclient.py
@@ -13,7 +13,7 @@ from twill import commands  # This will make possible go through the web
 
 # Threhsold on the number of proteins per files. If a file contains more than that,
 # it will be divided into smaller files.
-numProteinsPerFile = 5
+numProteinsPerFile = 2000
 
 # Everything entry that is longer than the threshold below will be discarded.
 lineLengthThreshold = 9000
@@ -40,7 +40,6 @@ goodRecords     = {}
 # We now split them into two dictionaries depedinding on their
 # size.
 for j in KEYS:
-    print len( f[j] )
     if len( f[j] ) > lineLengthThreshold:
         records2Discard[ j ] = f[j]
     else:
@@ -95,57 +94,61 @@ for k in range(0, numFiles):
 # the SignalP web app, wait for the results and save it in our
 # hard drive.
 
-# for fastafilename in resultfilenames:
-#
-#     inputfile = open( fastafilename , 'r' )
-#     inforead = inputfile.read()
-#
-#
-#     # First steps...
-#     print 'Accessing the web app and filling the form...'
-#
-#     # Going to the web app page
-#     commands.go('http://www.cbs.dtu.dk/services/SignalP/')
-#
-#     # Setting up a few options
-#     commands.fv('2','SEQPASTE',inforead)
-#     commands.fv('2','graphmode',[''])
-#
-#
-#     # Submiting the appropriate form
-#     print 'Submiting the form'
-#     commands.submit('13')
-#
-#
-#     # Guessing now the job id
-#     a = commands.show()
-#     b = a.split('\n')[2]
-#     jobid = b.split('of')[1].split('</span')[0].strip()
-#
-#     # Constructing the url with the results...
-#     url2go = 'http://www.cbs.dtu.dk//cgi-bin/webface2.fcgi?jobid=' + jobid
-#     print 'Our results are in ' + url2go + '\n\n'
-#
-#     # Waiting a bit for the results...
-#     print 'Waiting some moments for the server to complete our request!'
-#     time.sleep(5*60)
-#
-#     # This timing has actually two purposes: while the results are being
-#     # calculated, the script will be quiet. But also, there will be some
-#     # time after the results are ready so that the server won't go overloaded.
-#     # Please, do not overload the server using this script!!
-#
-#     # Going after the results...
-#     commands.go(url2go)
-#
-#
-#     # Following the link for the result file
-#     commands.follow('processed fasta entries')
-#
-#     # Saving the result in the hard drive
-#     commands.save_html( fastafilename.split('.fasta')[0] + '_RESULT.fasta' )
-#
-#
-#
-#
-# # The end, my friend.
+kid = 0
+
+for filename in resultingFileNames:
+
+    inputFasta = open( filename , 'r' )
+    inforead = inputFasta.read()
+
+    ## Communicating with the SignalP server
+
+    # First steps...
+    print 'Accessing the web app and filling the form...'
+
+    # Going to the web app page
+    commands.go('http://www.cbs.dtu.dk/services/SignalP/')
+    print commands.showforms()
+
+    # Setting up a few options
+    commands.fv('2', 'SEQPASTE', inforead)
+    commands.fv('2', 12, '')
+
+    # Submiting the appropriate form
+    print 'Submiting the form'
+    commands.submit('2')
+
+    # Guessing now the job id
+    a = commands.show()
+    print a
+    b = a.split('\n')[2]
+    jobid = b.split('of')[1].split('</span')[0].strip()
+
+    # Constructing the url with the results...
+    url2go = 'http://www.cbs.dtu.dk//cgi-bin/webface2.fcgi?jobid=' + jobid
+    print 'Our results are in ' + url2go + '\n\n'
+
+    # Waiting a bit for the results...
+    print 'Waiting some moments for the server to complete our request!'
+    time.sleep(5*60)
+
+    # This timing has actually two purposes: while the results are being
+    # calculated, the script will be quiet. But also, there will be some
+    # time after the results are ready so that the server won't go overloaded.
+    # Please, do not overload the server using this script!!
+
+    # Going after the results...
+    commands.go(url2go)
+
+
+    # Following the link for the result file
+    commands.follow('processed fasta entries')
+
+    # Saving the result in the hard drive
+    commands.save_html( parsedInputFile_prefix + '_SignalPclient_id' + str(kid) + '_RESULT.fasta' )
+    kid += 1
+
+
+
+
+# The end, my friend.
